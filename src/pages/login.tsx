e879199f4465768b206473fa4bba9577009e0396
Hugo Raposo <hugojoaoraposo@gmail.com>
2023-06-21 00:06:04 +0100

    worked on the background image of the login/sign up page

---
+++ src/pages/login.tsx
@@ -0,0 +1,15 @@
+import 'tailwindcss/tailwind.css';
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+      
+      <div className="bg-login-bg bg-cover bg-no-repeat bg-center min-h-screen">
+
+      
+
+      </div>
+    );
+  };
+
+  export default LoginPage;
