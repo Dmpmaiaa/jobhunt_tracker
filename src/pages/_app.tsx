08924fa35754891d7a27154c734cf8ef519170b6
Tomas <tomasgoncalo@hotmail.com>
2023-06-21 16:14:08 +0100

    Added Btn component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import React from "react";
+import type { AppProps } from "next/app";
+import Btn from "./components/Btn";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: React.FC = () => {
+  return (
+    <div className="h-[100vh] overflow-hidden w-full bg-corPrincipal flex flex-col gap-3 ">
+      <div className="">
+        <Btn onClick={() => console.log("CLIQUEI LOGIN")} text="Login" />
+      </div>
+      <div>
+        <Btn onClick={() => console.log("CLIQUEI SIGNUP")} text="Signup" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
